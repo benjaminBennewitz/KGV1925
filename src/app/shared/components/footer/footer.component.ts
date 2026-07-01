5f36d779baa2e8a012af8bd015cc2ec7ebe5ca30
benjaminBennewitz <benjamin.bennewitz@gmail.com>
2026-07-02 00:33:51 +0200

    feature(footer): add negative footer layout

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -4,6 +4,17 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HAUPTNAVIGATION } from '../../data/navigation.data';
 
+interface FooterLink {
+  label: string;
+  pfad: string;
+}
+
+interface SocialHinweis {
+  label: string;
+  kurzform: string;
+  hinweis: string;
+}
+
 @Component({
   selector: 'app-footer',
   imports: [RouterLink],
@@ -13,4 +24,37 @@ import { HAUPTNAVIGATION } from '../../data/navigation.data';
 export class FooterComponent {
   protected readonly navigation = HAUPTNAVIGATION;
   protected readonly jahr = new Date().getFullYear();
+
+  protected readonly rechtlicheLinks: FooterLink[] = [
+    {
+      label: 'Impressum',
+      pfad: '/impressum',
+    },
+    {
+      label: 'Datenschutz',
+      pfad: '/datenschutz',
+    },
+    {
+      label: 'Barrierefreiheit',
+      pfad: '/barrierefreiheit',
+    },
+  ];
+
+  protected readonly socialHinweise: SocialHinweis[] = [
+    {
+      label: 'Instagram',
+      kurzform: 'IG',
+      hinweis: 'Instagram-Profil folgt',
+    },
+    {
+      label: 'Facebook',
+      kurzform: 'FB',
+      hinweis: 'Facebook-Seite folgt',
+    },
+    {
+      label: 'YouTube',
+      kurzform: 'YT',
+      hinweis: 'YouTube-Kanal folgt',
+    },
+  ];
 }
